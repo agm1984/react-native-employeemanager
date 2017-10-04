ebf243a3398f1c59d20d9825ae7b3e5f55be3fc1
Adam Mackintosh <agm1984@gmail.com>
2017-10-04 02:19:56 -0700

    project initialized

--- src/App.js
+++ src/App.js
@@ -2,3 +2,31 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import reducers from './reducers'
+import firebase from 'firebase'
+
+class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyBq6mC5i1TnRWzc6OnR1Xeh1lTjeflpFsc',
+            authDomain: 'employeemanager-913ef.firebaseapp.com',
+            databaseURL: 'https://employeemanager-913ef.firebaseio.com',
+            projectId: 'employeemanager-913ef',
+            storageBucket: 'employeemanager-913ef.appspot.com',
+            messagingSenderId: '566648609210'
+        }
+
+        firebase.initializeApp(config)
+    }
+    render() {
+        return (
+            <Provider store={createStore(reducers)}>
+                <View>
+                    <Text>HELLO</Text>
+                </View>
+            </Provider>
+        )
+    }
+}
+
+export default App
